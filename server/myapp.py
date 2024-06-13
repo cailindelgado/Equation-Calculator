53a4e50a2db34a0584632482e92238371c4f9617
Cailin Delgado <c.delgado@uqconnect.edu.au>
2024-06-13 13:50:10 +1000

    worked on it a little more

--- server/myapp.py
+++ server/myapp.py
@@ -1,7 +1,7 @@
 from flask import *
 import json
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+app = Flask(__name__, static_folder='../static', static_url_path='/')
 
 @app.route('/api/data')
 def cats():
