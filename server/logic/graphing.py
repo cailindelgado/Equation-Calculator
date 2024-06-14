e906e018c351339007b8adf24e23eaf3ed23c18e
Cailin Delgado <c.delgado@uqconnect.edu.au>
2024-06-14 19:38:22 +1000

    moving to wsl

---
+++ server/logic/graphing.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+class graphing(object):
+    def __init__(self, equation: str) -> None:
+        pass
+
+    def point(x: int, y: int) -> None:
+        plt.plot(x, y)
+        plt.show
---
+++ server/logic/integration.py
@@ -0,0 +1,18 @@
+
+class integrator(object):
+    def __init__(self, equation: list[str]) -> None:
+        self.eqn = equation
+
+    def normal() -> str:
+        pass
+
+    def subst() -> str:
+        pass
+    
+    def byParts() -> str:
+        pass
+
+    def partialFrac() -> str:
+        pass
+
+
